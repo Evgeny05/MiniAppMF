244af633eab4d962f448228f631dc01b5e23e73c
Evgeny05 <ded@liis.su>
2023-10-24 13:18:30 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {ScrollView, Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <ScrollView>
+      <View style={{flexDirection: 'row'}}>
+        <Text>dcscs</Text>
+        <Text>dcscs</Text>
+      </View>
+    </ScrollView>
+  );
+}
+
+export default App;
